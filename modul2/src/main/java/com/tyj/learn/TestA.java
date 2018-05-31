37713831e3da4ca1edbcc4f64190b48bd81291ab
tyj0104 <254103715@qq.com>
2018-05-31 17:41:06 +0800

    aar合并

---
+++ modul2/src/main/java/com/tyj/learn/TestA.java
@@ -0,0 +1,13 @@
+package com.tyj.learn;
+
+/**
+ * Created by dev_tyj on 2018/5/31.
+ */
+
+public class TestA {
+    public static void print1(){
+        System.out.println(
+                "Module--2"
+        );
+    }
+}
---
+++ module1/src/main/java/com/tyj/learn/Test2.java
@@ -0,0 +1,13 @@
+package com.tyj.learn;
+
+/**
+ * Created by dev_tyj on 2018/5/31.
+ */
+
+public class Test2 {
+    public static void print2(){
+        System.out.println(
+                "Module--2"
+        );
+    }
+}
